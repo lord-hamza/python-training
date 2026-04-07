f851d31d458ec35b006f7f33896ed09262a0f954
lord-hamza <hamzashahbaz5703@Gmail.com>
2026-04-07 20:50:03 +0500

    Banking program with While Loop

--- functions/banking.py
+++ functions/banking.py
@@ -1,13 +1,15 @@
 # Banking Program
 
 def show_balance():
-    pass
+    print(f"Your balance is ${balance.2f}")
 
-def deposit():
-    pass
+def deposit(amount):
+    amount = int(input("Enter the amount to deposit: "))
+    amount += balance
 
-def withdraw():
-    pass
+def withdraw(amount):
+    amount = int(input("Enter the amount to withdraw: "))
+    amount -= balance
 
 balance = 0
 is_running = True
@@ -18,3 +20,16 @@ while is_running:
     print("2. Deposit")
     print("3. Withdraw")
     print("4. Exit")
+
+    choose = int(input("Enter your choice (1-4): "))
+
+    if choose == 1:
+        show_balance()
+    elif choose == 2:
+        deposit(amount)
+    elif choose == 3:
+        withdraw(amount)
+    elif choose == 4:
+        is_running = False
+    else:
+        print("Invalid choice! Choose the right option.")
