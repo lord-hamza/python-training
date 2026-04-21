958eb33aaf86282730c7cd17ff72abd0816cfc6b
lord-hamza <hamzashahbaz5703@Gmail.com>
2026-04-22 00:21:53 +0500

    basic banking code fixes

--- functions/banking.py
+++ functions/banking.py
@@ -1,35 +1,52 @@
 # Banking Program
 
-def show_balance():
-    print(f"Your balance is ${balance.2f}")
+def show_balance(balance):
+    print(f"Your balance is ${balance:.2f}")
 
-def deposit(amount):
+def deposit():
     amount = int(input("Enter the amount to deposit: "))
-    amount += balance
-
-def withdraw(amount):
-    amount = int(input("Enter the amount to withdraw: "))
-    amount -= balance
-
-balance = 0
-is_running = True
-
-while is_running:
-    print("Banking Program")
-    print("1. Show Balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Exit")
-
-    choose = int(input("Enter your choice (1-4): "))
-
-    if choose == 1:
-        show_balance()
-    elif choose == 2:
-        deposit(amount)
-    elif choose == 3:
-        withdraw(amount)
-    elif choose == 4:
-        is_running = False
+    if amount < 0:
+        print("Invalid amount! Please enter a positive number.")
     else:
-        print("Invalid choice! Choose the right option.")
+        return amount
+
+def withdraw(balance):
+    amount = int(input("Enter the amount to withdraw: "))
+    if amount > balance:
+        print("Insufficient funds!")
+        return 0
+    elif amount < 0:
+        print("Invalid amount! Please enter a positive number.")
+        return 0
+    else:
+        return amount
+
+
+def main():
+    balance = 0
+    is_running = True
+
+    while is_running:
+        print("Banking Program")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+
+        choose = int(input("Enter your choice (1-4): "))
+
+        if choose == 1:
+            show_balance(balance)
+        elif choose == 2:
+            balance += deposit()
+        elif choose == 3:
+            balance -= withdraw(balance)
+        elif choose == 4:
+            is_running = False
+        else:
+            print("Invalid choice! Choose the right option.")
+
+    print("Thank you!")
+
+if __name__ == "__main__":
+    main()
